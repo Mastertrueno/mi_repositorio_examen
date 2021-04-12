6a0314bc0cfa37681e4dfd2c119550eb12df5560
User <User@DESKTOP-907Q3LL>
2021-04-12 20:36:29 +0200

    +Persona.java

---
+++ src/UD5_Examen_Git/Persona.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UD5_Examen_Git;
+
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author User
+ */
+public class Persona {
+    protected String nombre;
+    protected String apellidos;
+    protected GregorianCalendar fechaNacim;
+}
